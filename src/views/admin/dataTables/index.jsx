9fddcf11d67a4bf97268fc5ac0bbe5ec837b305d
Iris <wiktoriajarema301@gmail.com>
2024-07-12 22:17:33 +0200

    mystaking added

--- src/views/admin/dataTables/index.jsx
+++ src/views/admin/dataTables/index.jsx
@@ -1,65 +1,33 @@
-/*!
-  _   _  ___  ____  ___ ________  _   _   _   _ ___   
- | | | |/ _ \|  _ \|_ _|__  / _ \| \ | | | | | |_ _| 
- | |_| | | | | |_) || |  / / | | |  \| | | | | || | 
- |  _  | |_| |  _ < | | / /| |_| | |\  | | |_| || |
- |_| |_|\___/|_| \_\___/____\___/|_| \_|  \___/|___|
-                                                                                                                                                                                                                                                                                                                                       
-=========================================================
-* Horizon UI - v1.1.0
-=========================================================
-
-* Product Page: https://www.horizon-ui.com/
-* Copyright 2023 Horizon UI (https://www.horizon-ui.com/)
-
-* Designed and Coded by Simmmple
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
-// Chakra imports
-import { Box, SimpleGrid } from "@chakra-ui/react";
-import DevelopmentTable from "views/admin/dataTables/components/DevelopmentTable";
-import CheckTable from "views/admin/dataTables/components/CheckTable";
-import ColumnsTable from "views/admin/dataTables/components/ColumnsTable";
-import ComplexTable from "views/admin/dataTables/components/ComplexTable";
-import {
-  columnsDataDevelopment,
-  columnsDataCheck,
-  columnsDataColumns,
-  columnsDataComplex,
-} from "views/admin/dataTables/variables/columnsData";
-import tableDataDevelopment from "views/admin/dataTables/variables/tableDataDevelopment.json";
-import tableDataCheck from "views/admin/dataTables/variables/tableDataCheck.json";
-import tableDataColumns from "views/admin/dataTables/variables/tableDataColumns.json";
-import tableDataComplex from "views/admin/dataTables/variables/tableDataComplex.json";
 import React from "react";
-
+import { Box, Flex } from "@chakra-ui/react";
+import Activestakes from "views/admin/dataTables/components/Activestakes";
+import AverageAPYChart from "views/admin/dataTables/components/AverageAPYChart";
+import PastStakes from "views/admin/dataTables/components/PastStakes";
+import TotalEarningsSummary from "views/admin/dataTables/components/TotalEarningsSummary";
+import ActiveVsClosedStakes from "views/admin/dataTables/components/ActiveVsClosedStakes";
 export default function Settings() {
   // Chakra Color Mode
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
-      <SimpleGrid
-        mb='20px'
-        columns={{ sm: 1, md: 2 }}
-        spacing={{ base: "20px", xl: "20px" }}>
-        <DevelopmentTable
-          columnsData={columnsDataDevelopment}
-          tableData={tableDataDevelopment}
-        />
-        <CheckTable columnsData={columnsDataCheck} tableData={tableDataCheck} />
-        <ColumnsTable
-          columnsData={columnsDataColumns}
-          tableData={tableDataColumns}
-        />
-        <ComplexTable
-          columnsData={columnsDataComplex}
-          tableData={tableDataComplex}
-        />
-      </SimpleGrid>
-    </Box>
-  );
+    <Flex justify="space-between" align="flex-start" wrap="wrap" mb="20px">
+      {/* Kwadraty po lewej */}
+      <Box width={{ base: "100%", md: "calc(50% - 10px)", xl: "calc(33.33% - 10px)" }} mb={{ base: "20px", md: 0 }}>
+        <TotalEarningsSummary />
+      </Box>
+      <Box width={{ base: "100%", md: "calc(50% - 10px)", xl: "calc(33.33% - 10px)" }} mb={{ base: "20px", md: 0 }}>
+        <AverageAPYChart />
+      </Box>
+      <Box width={{ base: "100%", md: "calc(50% - 10px)", xl: "calc(33.33% - 10px)" }} mb={{ base: "20px", md: 0 }}>
+        <ActiveVsClosedStakes />
+      </Box>
+      {/* Kwadraty po prawej */}
+      <Box width={{ base: "200%", md: "50%", xl: "60.66%" }}>
+        <Activestakes />
+      </Box>
+      <Box width={{ base: "100%", md: "50%", xl: "33.33%" }}>
+        <PastStakes />
+      </Box>
+    </Flex>
+  </Box>
+);
 }
